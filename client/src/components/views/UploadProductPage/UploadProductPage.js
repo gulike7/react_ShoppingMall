b7a332eb136d592954ad0a3992dc63786d6594f2
rnin9 <rkdalswn1209@kw.ac.kr>
2020-06-17 16:01:40 +0900

    ProductUploadPage_Image Upload Using Multer

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,51 +1,80 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Typography, Button, Form, Input} from 'antd';
-import TextArea from 'antd/lib/input/TextArea';
+import FileUpload from '../../utils/FileUpload'
+const{ TextArea } = Input;
 
-const{Title} = Typography;
-const{TextArea} = Input;
-
-    
+const Kinds =[
+    {key:1, value:"고데기"}, 
+    {key:2, value:"롤빗"},
+    {key:3, value:"샴푸"},
+    {key:4, value:"염색제"},
+    {key:5, value:"크림"},
+    {key:6, value:"트리트먼트"},
+    {key:7, value:"픽서"}
+]
 
 function UploadProductPage(){
     
-    const{Title, setTitle} = useState("")
+    const[Title, setTitle] = useState("")
+    const[Description, setDescription] = useState("")
+    const[Price, setPrice] = useState(0)
+    const[Kind, setKind] = useState(1) // 상품종류
+    const[Images,setImages] = useState([])
+    
 
 
-    const titleChangeHandler = () =>{
+    const titleChangeHandler = (event) =>{
+        setTitle(event.currentTarget.value)
     }
 
+    const descriptionChangeHandler = (event) =>{
+        setDescription(event.currentTarget.value)
+    }
 
+    const priceChangeHandler = (event) =>{
+        setPrice(event.currentTarget.value)
+    }
+
+    const kindChangeHandler = (event) =>{
+        setKind(event.currentTarget.value)
+    }
+
+    const imageChangeHandler = (event) =>{
+        setImages(event.currentTarget.value)
+    }
 
 
     return(
         <div style={{maxWidth: '700px', margin: '2rem auto'}}>
             <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
-                <Title level={2}>뷰티 상품 업로드</Title>
+                <h2>뷰티 상품 업로드</h2>
                  
             </div>
            
             <Form>
 
               {}
+
+              <FileUpload></FileUpload>
               <br/>
               <br/>
               <label>이름</label>
-              <Input onChange={titleChangeHandler}/>
+              <Input onChange={titleChangeHandler} value={Title}/>
               <br/>
               <br/>
               <label>Description</label>
-              <TextArea/>
+              <TextArea onChange={descriptionChangeHandler} value={Description}/>
               <br/>
               <br/>
-                            
-              <label>가격($)</label>
-              <Input/>
+              <label>가격(Won)</label>
+              <Input type="number" onChange={priceChangeHandler} value ={Price}/>
               <br/>
               <br/>
-              
-              <select>
-                  <option></option>
+              <select onChange={kindChangeHandler} value ={Kind}>
+                  {Kinds.map(item=> (
+                      <option key={item.key} value={item.key}>{item.value}</option>
+                  ))}
+                  
               </select>
               <br/>
               <br/>
