f9f855a31a19d183f9c660b9837b7fa1ef0b730a
rnin9 <rkdalswn1209@kw.ac.kr>
2020-06-18 15:36:34 +0900

    DetailProductPage Make Image Gallery

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import ProductImage from './Sections/ProductImage';
+import ProductInfo from './Sections/ProductInfo';
+import {Row, Col} from 'antd';
+
 
 function DetailProductPage(props) {
     const productId = props.match.params.productId
+
+    const [Product, setProduct] = useState({})
+
     useEffect(()=>{
         axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
             .then(response =>{
                 if(response.data.success){
-                    console.log(response.data)
+                    setProduct(response.data.product[0])
                 }else{
                     alert('상세정보 가져오기를 실패했습니다!')
                 }
@@ -15,8 +22,27 @@ function DetailProductPage(props) {
     })
     
     return (
-        <div>
-            Detaila1231
+        <div style ={{width: '100%', padding: '3rem 4rem'}}>
+
+            <div style={{ display:'flex', justifyContent: 'center'}}>
+                <h1>{Product.title}</h1>
+            </div>
+
+            <br/>
+            <Row gutter={[16,16]}>
+
+                <Col lg={12} sm={24}>
+                    <ProductImage detail={Product}/>
+                   
+                </Col>
+                <Col lg={12} sm={24}>
+                    <ProductInfo />
+                </Col>
+            </Row>
+            
+            
+
+           
         </div>
     )
 }
